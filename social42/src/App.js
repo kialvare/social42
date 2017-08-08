63cb7f8107303fec19f4056087175a7d4e07990e
Kim <kimalvarez88@gmail.com>
2017-08-07 17:37:18 -0700

    started connecting to Twitter API

--- social42/src/App.js
+++ social42/src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Post to Twitter.
         </p>
       </div>
     );
---
+++ social42/src/Twitter.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: 'b8fo9CCPnDbWh3VTQGxcMcJfF',
+	consumer_secret: 'DObfqiitBJWWEwsUHfeSLxzUOl237eDmPUc8Twih4TN74txyvt',
+	access_token_key: '727493824860311552-rigbM0RMnKQBS0c1768gV9m7aBRw0IS',
+	access_token_secret: 'H1V6iLgFuInDxvhc0HDYisoz71qqwmuchgYbiA9DAlBAp'
+});
