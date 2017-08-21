ce7d5a3da9f61667832a1ee706495bfa849ce0e6
Kim <kimalvarez88@gmail.com>
2017-08-20 19:25:14 -0700

    kim's push

--- social42/src/App.js
+++ social42/src/App.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import NavigationBar from './NavigationBar.js';
+
 class App extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {value: ''};
-
-		
-	}
-
 	render() {
 		return (
-			<div className="App">
-				<div className="App-header">
-				<h2 className="Title">Post to Twitter</h2>
-			</div>
-			<form onSubmit={this.handleSubmit}>
-				<label>
-				Tweet:
-				<br />
-				<input type="text" />
-				<br />
-				<input type="submit" value="Submit" />
-				</label>
-			</form>
-			</div>
+		<NavigationBar />
 		);
 	}
 }
---
+++ social42/src/NavButton.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+const styles = {
+	container: {
+		width: 200,
+		height: 80,
+		backgroundColor: '#35353B',
+	}
+	text: {
+		fontFamily: 'Raleway', 'Helvetica', 'sans serif';
+	}
+}
+
+export default class NavButton extends Component {
+	render () {
+		const { text } = this.props;
+		return (
+			<div style={styles.container}>
+				
+			</div>
+		);
+	}
+}
