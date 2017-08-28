b2e0e27c191191350badfd0651bda2970e68191e
Kim <kimalvarez88@gmail.com>
2017-08-27 21:59:54 -0700

    added

--- social42/server/index.js
+++ social42/server/index.js
@@ -3,31 +3,33 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../build/index.html'))
-})
+	res.render('home' , {
+		title: "Home"
+	});
+});
 
 app.get('/post', function (req, res) {
 	res.send('post')
-})
+});
 
 app.get('/tasks', function (req, res) {
 	res.send('tasks')
-})
+});
 
 app.get('/users', function (req, res) {
 	res.send('users')
-})
+});
 
 app.get('/calendar', function (req, res) {
 	res.send('calendar')
-})
+});
 
 app.get('/admin', function (req, res) {
 	res.send('admin')
-})
+});
 
 //app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../build/index.html')))
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
