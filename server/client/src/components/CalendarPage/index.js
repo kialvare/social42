262d7c49d927022130d7dfc10e8e02e1d634d44a
Greg Selbo <gselbo84@Greg-Selbos-MacBook-Pro.local>
2017-08-28 21:06:50 -0700

    For merging

--- server/client/src/components/CalendarPage/index.js
+++ server/client/src/components/CalendarPage/index.js
@@ -1,7 +1,66 @@
 import React, { Component } from 'react';
 import Layout from 'antd/lib/layout';
 import Breadcrumb from 'antd/lib/breadcrumb';
+import { Calendar } from 'antd';
 const { Content } = Layout;
+
+function getListData(value) {
+  let listData;
+  switch (value.date()) {
+    case 8:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'normal', content: 'This is usual event.' },
+      ]; break;
+    case 10:
+      listData = [
+        { type: 'warning', content: 'This is warning event.' },
+        { type: 'normal', content: 'This is usual event.' },
+        { type: 'error', content: 'This is error event.' },
+      ]; break;
+    case 15:
+      listData = [
+        { type: 'warning', content: 'This is warning event' },
+        { type: 'normal', content: 'This is very long usual event。。....' },
+        { type: 'error', content: 'This is error event 1.' },
+        { type: 'error', content: 'This is error event 2.' },
+        { type: 'error', content: 'This is error event 3.' },
+        { type: 'error', content: 'This is error event 4.' },
+      ]; break;
+    default:
+  }
+  return listData || [];
+}
+
+function dateCellRender(value) {
+  const listData = getListData(value);
+  return (
+    <ul className="events">
+      {
+        listData.map(item => (
+          <li key={item.content}>
+            <span className={`event-${item.type}`}>●</span>
+            {item.content}
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
+function getMonthData(value) {
+  if (value.month() === 8) {
+    return 1394;
+  }
+}
+
+function monthCellRender(value) {
+  const num = getMonthData(value);
+  return num ? <div className="notes-month">
+    <section>{num}</section>
+    <span>Backlog number</span>
+  </div> : null;
+}
 
 class CalendarPage extends Component {
 	render() {
@@ -11,9 +70,7 @@ class CalendarPage extends Component {
                     <Breadcrumb.Item>Page</Breadcrumb.Item>
                     <Breadcrumb.Item>Calendar</Breadcrumb.Item>
                 </Breadcrumb>
-                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
-                    Calendar Page
-                </div>
+                <Calendar dateCellRender={dateCellRender} monthCellRender={monthCellRender} />
             </Content>
 		);
 	}
